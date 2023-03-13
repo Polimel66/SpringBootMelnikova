da3d02ad3c2f5a17be236456697c44efd1571362
Polimel66 <melnikowapolia@yandex.ru>
2023-03-12 20:19:14 -0700

    First Commit

---
+++ src/main/java/com/example/springbootmelnikova/SpringBootMelnikovaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmelnikova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMelnikovaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMelnikovaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootmelnikova/SpringBootMelnikovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmelnikova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMelnikovaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
